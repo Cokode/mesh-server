81161ac85ef2e5e296892f0bcdf3172f9d35a5c6
Collins C. A. <contactmrc4@gmail.com>
2024-11-18 10:50:44 +0100

    - configured mongoose connection - registered Schemas - imported routes

--- Index.js
+++ Index.js
@@ -1,11 +1,39 @@
+import "./src/models/User.js"
+import "./src/models/Stash.js"; 
+import "./src/models/UserBoard.js";
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-
+import dotenv from "dotenv";
+import authRoutes from "./src/routes/authRoutes.js";
+import stashRoutes from "./src/routes/stashRoutes.js";
+import profileRoutes from "./src/routes/profileRoutes.js";
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(stashRoutes);
+app.use(profileRoutes);
+dotenv.config();
+
+mongoose.set("strictQuery", true);
+
+async function connectToDatabase() {
+  const connectionString = process.env.MONGO_URI;
+
+  try {
+        await mongoose.connect(connectionString, { // Todo: remove await
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Connected to MongoDB successfully");
+  } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+connectToDatabase();
 
 app.get('/', (req, res) => {
   res.send("hello Smith");
