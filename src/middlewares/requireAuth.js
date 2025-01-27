edf289ad585782bf23c48bdbb325c82261faf6ca
Collins C. A. <contactmrc4@gmail.com>
2025-01-27 12:02:39 +0100

    Update: Cleaned up comments

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -3,17 +3,24 @@ import jwt from "jsonwebtoken";
 const User = mongoose.model('User');
 import dotenv from "dotenv"; // maybe remove
 
+
 const requireAuth = (req, res, next) => {
   const {authorization} = req.headers;
 
+  console.log("I am in authorization...." + authorization)
+
   if (!authorization) {
+    console.log("authorization failed ....")
     return res.status(401).send({error : 'you must be logged in.'});
   }
 
   const token = authorization.replace('Bearer ', '');
 
+  console.log(token)
+
   jwt.verify(token, process.env.JWT_SECRET, async (err, payload) => {
     if (err) {
+      console.log("Token failed ....")
       console.log("error here in server");
       return res.status(401).send({error: 'you must be logged in'});
     }
