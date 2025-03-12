74fa6faa812042f1a15442f726ed9f5c713f78aa
Collins C. A. <contactmrc4@gmail.com>
2025-03-12 09:41:04 +0100

    Added: S3 Configuration file, upload and saveImage function to save image to AWS S3

---
+++ src/utils/saveImage.js
@@ -0,0 +1,24 @@
+import { uploadImageToS3 } from "./uploadImageToS3.js";
+
+export const saveImage = async (base64) => {
+
+  console.log("in SaveImage function.");
+  try {
+    // const { base64 } = req.body; // Get Base64 image from request
+
+    if (!base64) return res.status(400).json({ message: 'No image provided' });
+
+    const imageUrl = await uploadImageToS3(base64, "png"); // Upload to S3
+
+    return imageUrl;
+
+    // Save image URL in MongoDB
+   // const newImage = new ImageModel({ imageUrl });
+   // await newImage.save();
+
+    //res.json({ message: 'Image uploaded successfully', imageUrl });
+  } catch (error) {
+   // res.status(500).json({ message: 'Upload failed', error });
+   console.log(error);
+  }
+};
---
+++ src/utils/uploadImageToS3.js
@@ -0,0 +1,29 @@
+import s3 from './s3Config.js'; // Import S3 config
+import { v4 as uuidv4 } from 'uuid'; // Generate unique file names
+
+export const uploadImageToS3 = async (base64Image, fileType) => {
+  try {
+    // Extract image format (e.g., "jpeg", "png")
+    const matches = base64Image.match(/^data:image\/(\w+);base64,(.+)$/);
+    //if (!matches) throw new Error('Invalid Base64 image format');
+
+    //const ext = matches[1]; // Image extension
+    //const buffer = Buffer.from(matches[2], 'base64'); // Convert Base64 to Buffer
+    const buffer = Buffer.from(base64Image, 'base64');
+
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME, // S3 Bucket name
+      Key: `uploads/${uuidv4()}.${fileType}`, // Unique filename
+      Body: buffer,
+      ContentType: `image/${fileType}`,
+      // ACL: 'public-read', // Makes it publicly accessible
+    };
+
+    // Upload to S3
+    const uploadResult = await s3.upload(params).promise();
+    return uploadResult.Location; // Returns the file URL
+  } catch (error) {
+    console.error('S3 Upload Error:', error);
+    throw error;
+  }
+};
